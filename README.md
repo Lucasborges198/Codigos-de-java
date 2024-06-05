b1de3271f7ec1697cdcb19b199014b4f7306fa1c
lucasborgesribeiro198@gmail.com <109313461+Lucasborges198@users.noreply.github.com>
2024-06-05 19:07:51 -0300

    adicionei pela primeira vez algo no repositorio Git

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Codigos-de-java
 Codigos de java criados com o intuito de estudar a linguagem
+
+# estou testando as modificaçoes do git
---
+++ main.java
@@ -0,0 +1,10 @@
+
+public class main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        double conta = (double) y / x; // Convertendo y para double para evitar truncamento de inteiros
+        
+        System.out.println("O resultado da conta e: " + conta); // Corrigindo a chamada ao println
+    }
+}
